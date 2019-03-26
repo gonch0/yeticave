5cbf6b618d7c5bca6c7bb0181fe3a85cee51d7c8
gonch0 <42312277+gonch0@users.noreply.github.com>
2019-03-26 16:58:48 +0500

    Task 2-1

--- index.php
+++ index.php
@@ -3,7 +3,13 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Полковник'; 
 $user_avatar = 'img/user.jpg';
+
+
+
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -28,7 +34,7 @@ $user_avatar = 'img/user.jpg';
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-        <!-- здсь должен быть PHP код для показа имени пользователя -->
+        <!-- здсь должен быть PHP код для показа имени пользователя (задание 1-2) -->
         <?php if ($is_auth): ?>
             <div class="user-menu__image">
                 <img src=<?= $user_avatar?> width="40" height="40" alt="Пользователь">
@@ -66,25 +72,76 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
+            <!--заполните этот список из массива с товарами (Задание 2-2)-->
+
+            <?php 
+                $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+                // Создаем двумерный массив
+
+                $goods_list = [
+                    [
+                        'name' => '2014 Rossignol District Snowboard',
+                        'category' => $categories[0],
+                        'cost' => 10999,
+                        'url' => 'img/lot-1.jpg'
+                    ],
+                    [
+                        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+                        'category' => $categories[0],
+                        'cost' => 159999,
+                        'url' => 'img/lot-2.jpg'
+                    ],
+                    [
+                        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+                        'category' => $categories[1],
+                        'cost' => 8000,
+                        'url' => 'img/lot-3.jpg'
+                    ],                    
+                    [
+                        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+                        'category' => $categories[2],
+                        'cost' => 10999,
+                        'url' => 'img/lot-4.jpg'
+                    ],
+                    [
+                        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+                        'category' => $categories[3],
+                        'cost' => 7500,
+                        'url' => 'img/lot-5.jpg'
+                    ],
+                    [
+                        'name' => 'Маска Oakley Сanopy',
+                        'category' => $categories[5],
+                        'cost' => 5400,
+                        'url' => 'img/lot-6.jpg'
+                    ]
+                ];
+
+            ?>
+
+
+            <?php foreach ($goods_list as $key => $value): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$value['url']?>" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$value['category']?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['name']?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$value['cost']?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <?php endforeach; ?>
+
+
         </ul>
     </section>
 </main>
@@ -93,11 +150,22 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
+            <!--заполните этот список из массива категорий (задание 2-1)-->
             <li class="nav__item">
                 <a href="pages/all-lots.html">Название категории</a>
             </li>
+
+            <?php 
+                $index = 0;
+                $num_count = count($categories);
+            ?>
+        
+            <?php while($index < $num_count):?>
+            <li class="nav__item"><a href="pages/all-lots.html"> <?= $categories[$index]; ?> </a></li>                
+            <?php $index = $index + 1; ?>
+            <?php endwhile; ?>
         </ul>
+
     </nav>
     <div class="main-footer__bottom container">
         <div class="main-footer__copyright">
